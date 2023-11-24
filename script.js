1c61d719797e161f41bf51894aa48a05326ed550
Oyerogba Emmanuel Olatunbosun <emmanuelola961@gmail.com>
2023-11-24 01:40:09 -0800

    Updated

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ function toggleMenu() {
     }
   }
   
+
+  window.onscroll = function() {
+    stickyHeader();
+  };
+
+  var header = document.getElementById("header");
+  var sticky = header.offsetTop;
+
+  function stickyHeader() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  }
