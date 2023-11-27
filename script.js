e6253df3393925ef6d1c3943fcf48de0b90bde62
Oyerogba Emmanuel Olatunbosun <emmanuelola961@gmail.com>
2023-11-27 05:07:56 -0800

    New Update

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function toggleMenu() {
       menu.style.width = '0';
       content.style.marginLeft = '0';
     } else {
-      menu.style.width = '330px'; // Adjust this value based on your desired menu width
-      content.style.marginLeft = '330px';
+      menu.style.width = '320px';
+      content.style.marginLeft = '320px';
     }
   }
   
